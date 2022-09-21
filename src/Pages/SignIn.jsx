6a0c9f79b0575bc2d6bb0712977f14fb320cb81f
Deshitha Thilindra (ddthilindra) <59472990+ddthilindra@users.noreply.github.com>
2022-09-21 16:17:43 +0530

    Sigin updated

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -12,9 +12,9 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 import Paper from "@mui/material/Paper";
-import { ReactComponent as GoogleLogo } from '../assets/google.svg';
+import { ReactComponent as GoogleLogo } from "../assets/google.svg";
 import { useHistory } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
@@ -30,7 +30,7 @@ function Copyright(props) {
     >
       {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
-      EMPsys
+        EMPsys
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
@@ -76,7 +76,7 @@ export default function SignIn() {
         await axios
           .post(`http://localhost:8000/user/login`, userData)
           .then((res) => {
-          console.log(res)
+            console.log(res);
             if (res.status == 200 && res.data.success == true) {
               // isSuccess = true;
               localStorage.setItem("username", res.data.sub.firstName);
@@ -122,127 +122,138 @@ export default function SignIn() {
   }, []);
   return (
     <ThemeProvider theme={theme}>
-    <Paper elevation={3}  sx={{ m:6,pt:6,pb:6,pl:0,pr:0}}>
-      <Typography
-        variant="h4"
-        component="h1"
-        sx={{
-          textAlign: "center",
-          width: "100%",
-          mb: "1.5rem",
-          pb: { sm: "3rem" },
-        }}
-      >
-        Welcome To EMPsys!
-      </Typography>
-      <div className="logincontainer" style={{display:"flex"}}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
+      <Paper elevation={3} sx={{ m: 6, pt: 6, pb: 6, pl: 0, pr: 0 }}>
+        <Typography
+          variant="h4"
+          component="h1"
+          sx={{
+            textAlign: "center",
+            width: "100%",
+            mb: "1.5rem",
+            pb: { sm: "3rem" },
+          }}
+        >
+          Welcome To EMPsys!
+        </Typography>
+        <div className="logincontainer" style={{ display: "flex" }}>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
             <Box
-              component="form"
-              onSubmit={handelLogin}
-              noValidate
-              sx={{ mt: 1 }}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
             >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                onChange={(e) => setemail(e.target.value)}
-                value={email}
-                error={emailError}
-                helperText={emailHError}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                value={password}
-                onChange={(e) => setpassword(e.target.value)}
-                error={passwordError}
-                helperText={passwordHError}
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <Box
+                component="form"
+                onSubmit={handelLogin}
+                noValidate
+                sx={{ mt: 1 }}
               >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  {/* <Link href="#" variant="body2">
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={(e) => setemail(e.target.value)}
+                  value={email}
+                  error={emailError}
+                  helperText={emailHError}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  value={password}
+                  onChange={(e) => setpassword(e.target.value)}
+                  error={passwordError}
+                  helperText={passwordHError}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    {/* <Link href="#" variant="body2">
                     Forgot password?
                   </Link> */}
+                  </Grid>
+                  <Grid item>
+                    <Link href="#" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
+              </Box>
             </Box>
-          </Box>         
-        </Container>
-        <hr/>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Typography
-                    variant='h6'
-                    component='p'
-                    sx={{
-                      paddingLeft: { sm: '3rem' },
-                      mb: '1.5rem',
-                      textAlign: 'center',
-                    }}
-                  >
-                    EMPsys
-                  </Typography>
-                  <Box
-                    display='flex'
-                    flexDirection='column'
-                    sx={{ paddingLeft: { sm: '3rem' }, rowGap: '1rem' }}
-                  >
-                    {/* <OauthMuiLink href=''>
+          </Container>
+          <hr />
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Typography
+              variant="h2"
+              component="p"
+              sx={{
+                paddingLeft: { sm: "3rem" },
+                mt: "1rem",
+                textAlign: "center",
+              }}
+            >
+              EMPsys
+            </Typography>
+            <Typography
+              variant="h7"
+              component="p"
+              sx={{
+                paddingLeft: { sm: "3rem" },
+                mt: "1rem",
+                textAlign: "center",
+              }}
+            >
+              The EMPsys : Employee Management System
+            </Typography>
+            <Box
+              display="flex"
+              flexDirection="column"
+              sx={{ paddingLeft: { sm: "3rem" }, rowGap: "1rem" }}
+            >
+              {/* <OauthMuiLink href=''>
                       <GoogleLogo style={{ height: '2rem' }} />
                       Google
                     </OauthMuiLink> */}
-                    {/* <OauthMuiLink href=''>
+              {/* <OauthMuiLink href=''>
                       <GitHubLogo style={{ height: '2rem' }} />
                       GitHub
                     </OauthMuiLink> */}
-                  </Box>
-        </Container>
-      </div>      
-      <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Box>
+          </Container>
+        </div>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
       </Paper>
     </ThemeProvider>
   );
